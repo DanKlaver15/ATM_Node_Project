71707add5f52d6f3ed6fdb6084d930baafc0d857
DKlaver <dklaver15@gmail.com>
2021-01-27 20:12:24 -0600

    Added console prompt for the user to enter their PIN and then validate.  Also began building the mainMenu function.

--- index.js
+++ index.js
@@ -2,7 +2,29 @@
 
 // JS ATM Program Using Node
 
+const prompt = require("prompt-sync")();
 const atm = require("./atm");
 
-console.log(account);
-console.log(atm);
+console.log("Hello. Please enter your PIN");
+
+let enteredPin = prompt();
+while (!atm.validatePin(enteredPin)) {
+   console.log("I'm sorry, but the PIN you entered is incorrect. Please try again");
+   enteredPin = prompt();
+}
+
+if (atm.validatePin(enteredPin)) {
+   mainMenu();
+}
+
+function mainMenu() {
+   console.log("Welcome to the Main Menu.  Please choose from the following options:" + "\n" +
+   "Enter the word 'balance' to check your current balance" + "\n" +
+   "Enter the word 'withdraw' to make a withdrawl" + "\n" +
+   "Enter the word 'deposit' to make a deposit");
+   let displayOption = prompt();
+}
+
+
+
+console.log("Congrats, so far your program works");
